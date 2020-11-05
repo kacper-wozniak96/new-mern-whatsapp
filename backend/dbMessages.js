564cc77cb3756d7ce019bdaf8c72d135d2cc0a0d
Kacper Woźniak <starzynek2@gmail.com>
2020-11-05 15:40:23 +0100

    Complete backend

--- backend/dbMessages.js
+++ backend/dbMessages.js
@@ -4,4 +4,9 @@ const whatsappSchema = mongoose.Schema({
   message: String,
   name: String,
   timestamp: String,
+  received: Boolean,
 });
+
+const Messages = mongoose.model('messagecontent', whatsappSchema);
+
+export default Messages;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import Pusher from 'pusher';
+// eslint-disable-next-line import/extensions
+import Messages from './dbMessages.js';
 
 const app = express();
 const port = process.env.PORT || 9000;
 
+const pusher = new Pusher({
+  appId: '1102224',
+  key: '5b2bf77f4143a21211ba',
+  secret: 'af7d445a6056becd1a32',
+  cluster: 'eu',
+  useTLS: true,
+});
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
 const connection_url =
   'mongodb+srv://admin:FKS30RUEmOf5npRt@cluster0.gabj6.mongodb.net/whatsappdb?retryWrites=true&w=majority';
 
@@ -13,7 +31,52 @@ mongoose.connect(connection_url, {
   useCreateIndex: true,
 });
 
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('DB is connected');
+
+  const msgCollection = db.collection('messagecontents');
+  const changeStream = msgCollection.watch();
+
+  changeStream.on('change', (change) => {
+    console.log(change);
+
+    if (change.operationType === 'insert') {
+      const messageDetails = change.fullDocument;
+      pusher.trigger('messages', 'inserted', {
+        name: messageDetails.name,
+        message: messageDetails.message,
+      });
+    } else {
+      console.log('Error triggering Pusher');
+    }
+  });
+});
+
 app.get('/', (req, res) => res.status(200).send('hello world'));
+
+app.get('/messages/sync', (req, res) => {
+  Messages.find({}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post('/messages/new', (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Listening on localhost: ${port}`));
